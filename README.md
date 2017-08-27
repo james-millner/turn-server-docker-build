509f2e06c4ae77a38de0091b67168b01d75e6da9
Todd Lyons <todd.lyons@pgi.com>
2017-08-27 16:54:59 -0700

    Add TURN_ARGS to deploy script, document.
    Add TURN_* env variables to sample values.yaml helm chart.

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ A Docker container with the [Coturn TURN server](https://github.com/coturn/cotur
 Build with:
 docker build -t turnserver:0.1.0 .
 
-docker run -d -e -p 3478:3478 "TURN_USER=foo" -e "TURN_PASS=bar" -e "TURN_REALM=mydomain" mrballcb/turnserver
+docker run -d -p 3478:3478 -e "TURN_USER=foo" -e "TURN_PASS=bar" -e "TURN_REALM=mydomain" mrballcb/turnserver
 
 It is not really intended to be used standalone.  It is intended to be used in
 AWS, fronted by an ELB that is doing TLS with simple http on the backend into
 this container running in Kubernetes.
 
+The local cli interface is active.  To disable it, you need to add *-e TURN_ARGS="--no-cli"*
+to the commandline.
 
 Adapted from original work done by: Anastasia Zolochevska <anastasiia.zolochevska@gmail.com>
 
--- deploy-turnserver.sh
+++ deploy-turnserver.sh
@@ -1,8 +1,9 @@
-echo "User: $TURN_USER"
-echo "Pass: $TURN_PASS"
-echo "Realm: $TURN_REALM"
+echo "User: ${TURN_USER}"
+echo "Pass: ${TURN_PASS}"
+echo "Realm: ${TURN_REALM}"
+echo "Turn Server Args: '${TURN_ARGS}'"
 
-turnadmin -a -u $TURN_USER -p "$TURN_PASS" -r $TURN_REALM
+turnadmin -a -u ${TURN_USER} -p "${TURN_PASS}" -r ${TURN_REALM}
 
 CONFIG_FILE=/etc/turnserver/turnserver.conf
 
@@ -10,12 +11,11 @@ CONFIG_FILE=/etc/turnserver/turnserver.conf
 internalIp="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 externalIp="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-echo "listening-ip=$internalIp
-relay-ip=$internalIp
-external-ip=$externalIp
+echo "listening-ip=${internalIp}
+relay-ip=${internalIp}
+external-ip=${externalIp}
 
-realm=$TURN_REALM" >> ${CONFIG_FILE}
+realm=${TURN_REALM}" >> ${CONFIG_FILE}
 
-exec /usr/bin/turnserver -c ${CONFIG_FILE} --no-cli
+/usr/bin/turnserver -c ${CONFIG_FILE} ${TURN_ARGS}
 
-#echo "TURN server running. IP: "$externalIp" Username: $TURN_USER, password: $TURN_PASS"
